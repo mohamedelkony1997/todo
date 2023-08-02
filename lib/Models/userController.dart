293a11dfc75a7ca130f50aeee16a8fcfd5e28be9
RAM <mohamedelkony1997>
2023-08-02 03:15:51 +0200

    ff

--- lib/Models/userController.dart
+++ lib/Models/userController.dart
@@ -13,7 +13,7 @@ class UserController {
   static const String baseUrl =
       'https://phpstack-561490-3524079.cloudwaysapps.com/api-start-point/public/api/auth/login';
 
-  Future<void> login(User user) async {
+ Future<void> login(User user) async {
     final response = await http.post(
       Uri.parse(baseUrl),
       body: {
@@ -21,46 +21,50 @@ class UserController {
         "password": user.password,
       },
     );
+    
     print(response.statusCode);
-     var extractedlogin = json.decode(response.body);
-    if (response.statusCode == 200) {
-      Get.to(HomeView());
-      var extractedlogin = json.decode(response.body);
-      print(extractedlogin);
-      Fluttertoast.showToast(
-          msg: "${extractedlogin["message"]}",
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.BOTTOM,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.black,
-          textColor: Colors.white,
-          fontSize: 16.0);
-      SharedPreferences preferences = await SharedPreferences.getInstance();
-      preferences.setString("token", extractedlogin["data"]["token"]);
-
-      print(extractedlogin["data"]["token"]);
-    }
-    else if (response.statusCode == 400 ) {
-      Fluttertoast.showToast(
-          msg: "${extractedlogin["message"]}",
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.BOTTOM,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.black,
-          textColor: Colors.white,
-          fontSize: 16.0);
-      print('Login failed: ${extractedlogin["message"]}');
+    
+    if (response.statusCode == 302) {
+        // If the response is a redirect, follow the redirect and try again
+        final redirectUrl = response.headers['location'];
+        final redirectResponse = await http.post(
+            Uri.parse(redirectUrl!),
+            body: {
+                "email": user.email,
+                "password": user.password,
+            },
+        );
+        // Handle the response from the redirected URL
+        // ...
+    } else if (response.statusCode == 200) {
+        // If the response status code is 200, parse the response as JSON
+        var extractedlogin = json.decode(response.body);
+        // Handle the JSON response
+        // ...
+    } else if (response.statusCode == 400 ) {
+        // If the response status code is 400, handle the error
+        var extractedlogin = json.decode(response.body);
+        Fluttertoast.showToast(
+            msg: "${extractedlogin["message"]}",
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.black,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        print('Login failed: ${extractedlogin["message"]}');
     } else {
-      Fluttertoast.showToast(
-          msg: "An error occurred. Please try again later.'",
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.BOTTOM,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.black,
-          textColor: Colors.white,
-          fontSize: 16.0);
+        // If the response status code is not 200 or 400, handle the error
+        Fluttertoast.showToast(
+            msg: "An error occurred. Please try again later.'",
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.black,
+            textColor: Colors.white,
+            fontSize: 16.0);
     }
-  }
+}
 
   Future<void> refreshToken() async {
     final preferences = await SharedPreferences.getInstance();
